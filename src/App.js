c9df332fd5c490f1a5b11986a85d2a6153376102
Henry <igwehenry740@gmail.com>
2024-01-17 21:25:42 +0100

    completede

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import WeatherWidget from "./Components/WeatherWidget";
-
+import { useState } from "react";
+import "./index.css";
 function App() {
+  const [inputValue, setInputValue] = useState("London");
+  const [location, setLocation] = useState("London");
+
+  const handleInput = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+    setLocation(inputValue);
+  };
+
   return (
-    <div>
-      <WeatherWidget />
+    <div className="app">
+      <WeatherWidget location={location} />
+      <form onSubmit={submitHandle}>
+        <input type="text" value={inputValue} onChange={handleInput} />
+        <button>update Location</button>
+      </form>
     </div>
   );
 }
--- src/Components/WeatherWidget.js
+++ src/Components/WeatherWidget.js
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 import axios from "axios";
 import { ReactComponent as MoonSvg } from "../Assets/Svgs/dark_mode_FILL1_wght400_GRAD0_opsz24.svg";
 import { ReactComponent as CloundSvg } from "../Assets/Svgs/cloud_FILL1_wght400_GRAD0_opsz24.svg";
 
-const WeatherWidget = () => {
+const WeatherWidget = ({ location }) => {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    if (location.length) {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=edc77ed54f4e82e341af2902d9068fea&units=metric`
+        )
+        .then((response) => {
+          setWeatherData(response.data);
+        })
+        .catch((error) => {
+          console.error("error: ", error);
+        });
+    }
+  }, [location]);
+
+  if (!weatherData) return <div>Loading ...</div>;
+  const currentTimeUTC = new Date().getTime() / 1000;
+  const localTime = currentTimeUTC + weatherData.timezone;
+  const isDayTime =
+    localTime > weatherData.sys.sunrise && localTime < weatherData.sys.sunset;
+  const cloudSize = weatherData.clouds.all;
+
   return (
     <div className="weather-widget">
-      <MoonSvg className="moon" />
+      {isDayTime ? <div className="sun"></div> : <MoonSvg className="moon" />}
+      {/* <div className="sun"></div> */}
       <div className="cloud-container">
-        <CloundSvg className="cloud" />
+        {cloudSize > 50 && <CloundSvg className="cloud" />}
       </div>
-      <div className="temperature">Temperature</div>
-      <div className="weather">weather</div>
-      <div className="low-high">low-high</div>
-      <div className="feels-like">feels-like</div>
-      <div className="location">location</div>
-      <div className="humidity">humidity</div>
+      <div className="temperature">{Math.round(weatherData.main.temp)}°</div>
+      <div className="weather">{weatherData.weather[0].main}</div>
+      <div className="low-high">
+        {Math.round(weatherData.main.temp_min)}° /{" "}
+        {Math.round(weatherData.main.temp_max)}°
+      </div>
+      <div className="feels-like">
+        feels-like: {Math.round(weatherData.main.feels_like)}°
+      </div>
+      <div className="location">{weatherData.name}</div>
+      <div className="humidity">Humidity: {weatherData.main.humidity}</div>
     </div>
   );
 };
